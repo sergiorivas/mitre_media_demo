843af58bd17cce36f35113dcfb71981fc4680ee4
Sergio Rivas <sergiorivas@gmail.com>
2023-02-15 16:18:55 -0500

    feat: adding analized field

---
+++ sentences_app/db/migrate/20230215211733_add_analized_to_sentence.rb
@@ -0,0 +1,5 @@
+class AddAnalizedToSentence < ActiveRecord::Migration[7.0]
+  def change
+    add_column :sentences, :analized, :boolean, default: false
+  end
+end
--- sentences_app/db/schema.rb
+++ sentences_app/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_15_165610) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_15_211733) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -36,6 +36,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_15_165610) do
     t.string "text", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "analized", default: false
   end
 
 end
