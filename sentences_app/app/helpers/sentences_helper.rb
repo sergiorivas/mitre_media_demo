b50bf6ae995e5f5cf3c103590cea077858878bb8
Sergio Rivas <sergiorivas@gmail.com>
2023-02-26 07:44:03 -0600

    feat: improving format

--- sentences_app/app/helpers/sentences_helper.rb
+++ sentences_app/app/helpers/sentences_helper.rb
@@ -2,6 +2,13 @@ module SentencesHelper
   def decorate_sentence_part(sentence_part)
     return sentence_part.text unless sentence_part.entity?
 
-    "<span class='rounded-lg border-black py-2 ml-1 px-3 bg-gray-100 font-semibold'>#{sentence_part.text}<span class='ml-2 font-bold' style='color: ##{sentence_part.entity_type.color}'>#{sentence_part.entity_type.name}</span></span>".html_safe
+    %{
+      <span class='rounded-lg border-black py-2 ml-1 px-3 bg-gray-100 font-semibold'>
+        #{sentence_part.text}
+        <span class='ml-2 font-bold' style='color: ##{sentence_part.entity_type.color}'>
+          #{sentence_part.entity_type.name}
+        </span>
+      </span>
+    }.html_safe
   end
 end
