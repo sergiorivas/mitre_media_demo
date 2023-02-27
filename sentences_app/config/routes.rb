69cf77130635b3e012a389a56c956a60a8cebb4a
Sergio Rivas <sergiorivas@gmail.com>
2023-02-27 11:34:42 -0500

    chore: clean routes

--- sentences_app/config/routes.rb
+++ sentences_app/config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   resources :sentences, only: [:new, :create, :edit, :update, :index, :destroy]
   resources :sentence_parts, only: [:update]
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
   root "sentences#index"
 end
